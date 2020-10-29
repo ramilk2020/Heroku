eca0e64695e23b4fbd25dd318788e71719906764
ramilk2020 <ramil.kirner@gmail.com>
2020-10-29 19:41:07 +0100

    Pre

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
+from flask import Flask, jsonify
 import os
-from flask import Flask
-from models import setup_db
-from flask_cors import CORS
-
-def create_app(test_config=None):
-
-    app = Flask(__name__)
-    setup_db(app)
-    CORS(app)
-
-    @app.route('/')
-    def get_greeting():
-        excited = True #os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
-
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
-
-    return app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
+from utility import make_get_request
+app = Flask(__name__)
+  @app.route('/', methods=['GET'])
+  def hello():
+      return 'Hello world, almost there!'
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
