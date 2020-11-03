8defdc05319bdeb183cbca751f68c1c352baafe2
ramilk2020 <ramil.kirner@gmail.com>
2020-11-03 21:59:00 +0100

    H2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, Actors, Movies
 from flask_cors import CORS
 
 def create_app(test_config=None):
@@ -9,8 +10,12 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    CORS(app, resources={r"/": {"origins": "*"}})
+
     @app.route('/')
     def get_greeting():
+        if 'EXCITED' not in os.environ:
+          os.environ['EXCITED'] = "true"
         excited = os.environ['EXCITED']
         greeting = "Hello" 
         if excited == 'true': greeting = greeting + "!!!!!"
@@ -19,6 +24,43 @@ def create_app(test_config=None):
     @app.route('/coolkids')
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad! Yeppppieeee!"
+
+
+
+    # Get all actors
+    @app.route('/actors', methods=['GET'])
+    def get_actors():
+        actors = Actors.query.all()
+
+        if len(actors) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'actors': [actor.format() for actor in actors]
+        })
+    
+    # Create new actor 
+    @app.route('/actors', methods=['POST'])
+    def create_actor():
+        body = request.get_json()
+
+        if (('name' not in body) or ('age' not in body) or ('gender' not in body)):
+
+           abort(422)
+
+        name = body.get('name', None)
+        age = body.get('age', None)
+        gender = body.get('gender', None)
+
+        actor = Actors(name, age, gender)
+        actor.insert()
+
+        return jsonify({
+            'success': True #,
+            # 'actors': [actor.format() for actor in actors]
+        })
+    
 
     return app
 
--- models.py
+++ models.py
@@ -1,43 +1,95 @@
-from sqlalchemy import Column, String, Integer, create_engine
+import os
 from flask_sqlalchemy import SQLAlchemy
-import json
+from sqlalchemy import Column, String, Integer, DateTime
 
-#database_path = os.environ['DATABASE_URL']
-#database_name = "trivia"
-# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-database_path = "postgres://xziznhghlbhwox:250ea74c53494d88192282efc13e6676da8b73faba05544ccb97206ba2f55381@ec2-54-156-149-189.compute-1.amazonaws.com:5432/d1mqqn6tjiljlh"
+# --------------------------------------------
+# CONFIG
+# --------------------------------------------
+
+if 'DATABASE_URL' not in os.environ:
+  database_name = "casting"
+  database_path = "postgres://{}:{}@{}/{}".format('postgres','rammel','localhost:5432', database_name)
+else:
+  database_path = "postgres://xziznhghlbhwox:250ea74c53494d88192282efc13e6676da8b73faba05544ccb97206ba2f55381@ec2-54-156-149-189.compute-1.amazonaws.com:5432/d1mqqn6tjiljlh"
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
+
     db.init_app(app)
     db.create_all()
 
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
+# --------------------------------------------
+# MODELS
+# --------------------------------------------
+
+class Actors(db.Model):  
+  __tablename__ = 'Actors'
 
   id = Column(Integer, primary_key=True)
   name = Column(String)
-  catchphrase = Column(String)
+  age = Column(Integer)
+  gender = Column(String)
 
-  def __init__(self, name, catchphrase=""):
+  def __init__(self, name, age, gender):
     self.name = name
-    self.catchphrase = catchphrase
+    self.age = age
+    self.gender = gender
+  
+  # def __repr__(self):
+  #   return 'This is the __repr__ method of Actors table'
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'catchphrase': self.catchphrase}
+      'age': self.age,
+      'gender': self.gender
+    }
+
+class Movies(db.Model):  
+  __tablename__ = 'Movies'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release = Column(DateTime)
+
+  def __init__(self, title, release):
+    self.title = title
+    self.release = release
+
+  def __repr__(self):
+    return 'This is the __repr__ method of Movies table'
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+    
+  def update(self):
+    self.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release': self.release
+  }
